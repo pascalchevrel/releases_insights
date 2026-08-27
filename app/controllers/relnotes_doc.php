ca79f5a7373cb832191717e8501457368dea90eb
Donal Meehan <76789979+DonalMe@users.noreply.github.com>
2026-08-27 18:28:16 -0400

    Switch release notes draft doc link to 156

--- app/controllers/relnotes_doc.php
+++ app/controllers/relnotes_doc.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 // Link to the current Release Notes draft doc. Update that link every cycle.
-$doc = 'https://docs.google.com/document/d/1_Pm4yelKsjGZkj8DV3X-w8WyvAN8EcmnScEEgbaFFAU/edit?usp=sharing';
+$doc = 'https://docs.google.com/document/d/1i_dQ993vGYS___EJtTd72bWidi8TNQz_RBvNfs4ow78/edit?usp=sharing';
 
 header("Location: $doc", true, 302);
 exit;
